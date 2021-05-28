203e8ef95c937f93ed8b1144b008aec2043b7839
Batoolayyad <batoolayyad1996@gmail.com>
2021-05-28 23:13:00 +0300

    finish lab 8

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-
+import Movie from './components/Movie';
 
 class App extends React.Component {
   constructor(props) {
@@ -14,7 +14,9 @@ class App extends React.Component {
       showMap: false,
       errorMessage: false,
       forecastArrFront: [],
-      showWeather: false
+      showWeather: false,
+      movieArr: [],
+      showMovie: false
     }
   }
 
@@ -23,7 +25,7 @@ class App extends React.Component {
     event.preventDefault();
     let serverRoute = process.env.REACT_APP_SERVER;
     let loctionUrl = `https://eu1.locationiq.com/v1/search.php?key=pk.6f781cde9385bcbeedf3a1bda9571332&q=${this.state.searchQuery}&format=json`;
-
+    //location
     try {
 
       let locationResult = await axios.get(loctionUrl);
@@ -41,14 +43,14 @@ class App extends React.Component {
       })
     }
 
-
+    //weather
     //http://localhost:3001/weather?city_name=Amman
     try {
-      console.log(serverRoute);
-      const url = `${serverRoute}weather?city_name=${this.state.searchQuery}`;
-       
-      const weatherData = await axios.get(url);
-      console.log(weatherData.data);
+      // console.log(serverRoute);
+      const weatherUrl = `${serverRoute}/weather?city_name=${this.state.searchQuery}`;
+
+      const weatherData = await axios.get(weatherUrl);
+      // console.log(weatherData.data);
 
 
       this.setState({
@@ -62,50 +64,88 @@ class App extends React.Component {
 
         showWeather: false
       })
-      console.log("saeed will kill me")
+      console.log('erroe from weather')
     }
 
-  }
 
+
+    //movies
+    try {
+      console.log(serverRoute);
+      const movieUrl = `${serverRoute}/movie?city_name=${this.state.searchQuery}`;
+
+      const movieData = await axios.get(movieUrl);
+      // console.log(movieData.data);
+
+
+      this.setState({
+        movieArr: movieData.data,
+        showMovie: true
+      })
+      console.log(this.state.movieArr)
+    }
+    catch (errors) {
+      // console.log(errors);
+      this.setState({
+
+        showMovie: false
+      })
+      console.log('error from move')
+    }
+    
+  }
   updateSearchQuery = (event) => {
     this.setState({
       searchQuery: event.target.value
     })
-    // console.log(this.state.searchQuery);
   }
+  // console.log(this.state.searchQuery);
+  //end of the findLocation function
 
 
 
   render() {
     return (
       <>
-        <h1>City Explorer</h1>
-        <Form onSubmit={this.findLocation}>
-          <Form.Group controlId="formBasicEmail" >
-            <Form.Label>City Name</Form.Label>
-            <Form.Control type="text" placeholder=" City Name" onChange={this.updateSearchQuery} />
-            <Form.Text className="text-muted">
-              Type the City Name
+        <div className="formDiv">
+          <h1>City Explorer</h1>
+          <Form onSubmit={this.findLocation}>
+            <Form.Group controlId="formBasicEmail" >
+              <Form.Label>City Name</Form.Label>
+              <Form.Control type="text" placeholder=" City Name" onChange={this.updateSearchQuery} />
+              <Form.Text className="text-muted">
+                Type the City Name
     </Form.Text>
-          </Form.Group>
-          <Button variant="primary" type="submit"  >
-            Find Location
+            </Form.Group>
+            <Button variant="primary" type="submit"  >
+              Find Location
   </Button>
-        </Form>
+          </Form>
+        </div>
 
-        <p>{this.state.locationData.display_name}</p>
-        { this.state.showMap &&
-          <img
-            src={`https://maps.locationiq.com/v3/staticmap?key=pk.6f781cde9385bcbeedf3a1bda9571332&center=${this.state.locationData.lat},${this.state.locationData.lon}&zoom=<zoom>&size=<width>x<height>&format=<format>&maptype=<MapType>&markers=icon:<icon>|<latitude>,<longitude>&markers=icon:<icon>|<latitude>,<longitude>`} alt=''
-          />
-        }
 
-        { this.state.forecastArrFront.map((item, idx) => {
-          return <p key={idx}>{item.date} and {item.description}</p>
+        <div className="locationDiv">
 
-        })}
-        <p>{this.state.item}</p>
+          <p>{this.state.locationData.display_name}</p>
+          {this.state.showMap &&
+            <img
+              src={`https://maps.locationiq.com/v3/staticmap?key=pk.6f781cde9385bcbeedf3a1bda9571332&center=${this.state.locationData.lat},${this.state.locationData.lon}&zoom=<zoom>&size=<width>x<height>&format=<format>&maptype=<MapType>&markers=icon:<icon>|<latitude>,<longitude>&markers=icon:<icon>|<latitude>,<longitude>`} alt=''
+            />
+          }
+        </div>
+        <div className="weatherDiv">
 
+          {this.state.forecastArrFront.map((item, idx) => {
+            return <p key={idx}>{item.date} and {item.description}</p>
+
+          })}
+          <p>{this.state.item}</p>
+        </div>
+
+        <div className="movieDiv">
+          {this.state.showMovie === true &&
+            <Movie display={this.state.showMovie} addMovie={this.state.movieArr} />}
+        </div>
       </>
     )
 
